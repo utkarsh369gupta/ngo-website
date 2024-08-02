77b884eadc4c65d48802c1749e3c7ce667d5e1a1
Utkarsh369Gupta <utk369gupta@gmail.com>
2024-08-02 23:52:42 +0530

    added mobile response 1

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,23 +1,24 @@
-// components/Navbar.tsx
 import React from 'react';
 
 export const Navbar: React.FC = () => {
     return (
-        <div className='flex bg-black p-3 pb-0 pl-10'>
-            <div className='flex-none w-32'>
+        <div className='flex flex-col md:flex-row bg-black p-3 pb-0 md:pl-10'>
+            <div className='hidden md:block flex-none w-32'>
                 <img src="images/logo.png" alt="Logo" className="w-32 h-32 mb-4 md:mb-0" />
             </div>
             <div className='flex-none w-full'>
-                <div className="bg-black p-5 text-white flex flex-col md:flex-row justify-around items-center pl-64">
-                    <div className="flex items-center mb-4 md:mb-0">
-                        <i className="fas fa-envelope mr-2"></i>
-                        writeus@hearthealing.org
+                <div className="bg-black p-2 md:p-5 text-white flex flex-col md:flex-row justify-around items-center md:pl-64">
+                    <div className="flex flex-col md:flex-row items-center mb-2 md:mb-0 text-sm md:text-base space-y-1 md:space-y-0 md:space-x-4">
+                        <div className="flex items-center">
+                            <i className="fas fa-envelope mr-2"></i>
+                            writeus@hearthealing.org
+                        </div>
+                        <div className="flex items-center">
+                            <i className="fas fa-phone mr-2"></i>
+                            +91 9999999999
+                        </div>
                     </div>
-                    <div className="flex items-center mb-4 md:mb-0">
-                        <i className="fas fa-phone mr-2"></i>
-                        +91 9999999999
-                    </div>
-                    <div className="flex items-center space-x-4 mb-4 md:mb-0">
+                    <div className="flex items-center space-x-2 md:space-x-4 mb-2 md:mb-0 text-sm md:text-base">
                         <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
                             <i className="fab fa-instagram"></i>
                         </a>
@@ -32,30 +33,27 @@ export const Navbar: React.FC = () => {
                         </a>
                     </div>
                 </div>
-                <div className="flex flex-col md:flex-row justify-around items-center bg-gray-200 p-3">
-
-                    <div className="flex ">
-                        <div className='justify-start'>
-                            <button className="bg-blue-100 hover:bg-blue-700 text-blue-500 font-bold py-2 px-4 rounded">
-                                Home
-                            </button>
-                            <button className="bg-blue-100 hover:bg-blue-700 text-blue-500 font-bold py-2 px-4 rounded">
-                                Who Are We
-                            </button>
-                            <button className="bg-blue-100 hover:bg-blue-700 text-blue-500 font-bold py-2 px-4 rounded">
-                                What We Do
-                            </button>
-                            <button className="bg-blue-100 hover:bg-blue-700 text-blue-500 font-bold py-2 px-4 rounded">
-                                Get Involved
-                            </button>
-                            <button className="bg-blue-100 hover:bg-blue-700 text-blue-500 font-bold py-2 px-4 rounded">
-                                Resources
-                            </button>
-                        </div>
-                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-3xl">
-                            Donate Now
+                <div className="flex flex-col md:flex-row justify-around items-center bg-gray-200 p-2 md:p-3">
+                    <div className="flex flex-col md:flex-row justify-start space-y-1 md:space-y-0 md:space-x-2">
+                        <button className="bg-blue-100 hover:bg-blue-700 text-blue-500 font-bold py-1 px-2 md:py-2 md:px-4 rounded">
+                            Home
+                        </button>
+                        <button className="bg-blue-100 hover:bg-blue-700 text-blue-500 font-bold py-1 px-2 md:py-2 md:px-4 rounded">
+                            Who Are We
+                        </button>
+                        <button className="bg-blue-100 hover:bg-blue-700 text-blue-500 font-bold py-1 px-2 md:py-2 md:px-4 rounded">
+                            What We Do
+                        </button>
+                        <button className="bg-blue-100 hover:bg-blue-700 text-blue-500 font-bold py-1 px-2 md:py-2 md:px-4 rounded">
+                            Get Involved
+                        </button>
+                        <button className="bg-blue-100 hover:bg-blue-700 text-blue-500 font-bold py-1 px-2 md:py-2 md:px-4 rounded">
+                            Resources
                         </button>
                     </div>
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 md:py-2 md:px-4 rounded-3xl mt-2 md:mt-0">
+                        Donate Now
+                    </button>
                 </div>
             </div>
         </div>
